b873fb6cec1c2c7c9ffc73cbe43bbbb2dc3c019a
Niwa <971382989@qq.com>
2017-11-04 21:12:38 +0800

    Initial commit

---
+++ app/src/main/java/cn/minminaya/privatetestproject/MainActivity.java
@@ -0,0 +1,38 @@
+package cn.minminaya.privatetestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_zhihu_ad)
+    Button btnZhihuAd;
+    @BindView(R.id.btn_null)
+    Button btnNull;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_zhihu_ad, R.id.btn_null})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_zhihu_ad:
+                Toast.makeText(this, "知乎滑动广告", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_null:
+                Toast.makeText(this, "null", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
